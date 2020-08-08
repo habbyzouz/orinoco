0ce55d17d71b163717b69b20072b3ea12abe2999
habbye <monyh@hotmail.fr>
2020-08-08 22:03:16 +0200

    banniere

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 /* navbar*/
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function() {
-    mainNav.classList.toggle('active');
-});
+window.onload = () => {
+    let mainNav = document.getElementById('js-menu');
+    let navBarToggle = document.getElementById('js-navbar-toggle');
+
+    navBarToggle.addEventListener('click', function() {
+        mainNav.classList.toggle('active');
+    });
+}
